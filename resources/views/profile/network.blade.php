ebea996a1eae5b406009f570369844d75128892f
exnnwc <2buzezsn@gmail.com>
2016-05-04 18:05:05 -0700

    network.blade.php overwritten

--- resources/views/profile/network.blade.php
+++ resources/views/profile/network.blade.php
@@ -1,8 +1,25 @@
 @extends ('master')
 @section ('content')
-    @foreach ($profile as $profile)
-    @endforeach
     @include ("profile.menu", ["route_name"=>Route::getCurrentRoute()->getName()])
+<div class='clear' style='margin-bottom:16px;'>
+<input type='button' id='showRelationshipTypes' class='textButton clear' value='[ Show Relationship Types ]' />
+<input type='button' id='hideRelationshipTypes' class='textButton clear' value='[ Hide Relationship Types ]' />
+</div>
+<div id='listOfRelationshipTypes' class='clear'>
+<form method="POST" action="{{ route('RelationshipType.store') }}">
+    {{ csrf_field() }}
+    <input type='text' name='newRelationshipTypeName' />
+    <input type='submit' value='New Relationship Type' />
+</form>
+@foreach ($relationship_types as $relationship_type)
+    <form method="POST" action="{{ route('RelationshipType.destroy', ['id'=>$relationship_type->id]) }}">
+    {{ csrf_field() }}
+    {{ method_field('DELETE') }}
+    <input type='submit' value='x' class='textButton red' />
+    {{ $relationship_type->name }}
+    </form>
+@endforeach
+</div>
     <h1 class='profileHeading'>
         Social Network 
     </h1>
@@ -27,6 +44,7 @@
     </form>
     @foreach ($people as $person)
         <div class='personListing'>
+            <div style='float:left;'>
             <a name='person{{$person->id}}'></a>
             <a href="{{ route('summary', ['person_id'=>$person->id]) }}">
                 @if (empty($person->name))
@@ -35,10 +53,35 @@
                     {{$person->name}}
                 @endif
             </a>
+            </div>
+            @foreach ($relationships as $relationship)
+                @if ( $relationship->secondary_person_id == $person->id)
+                    <form method="POST" action="{{ route('relationship.destroy', ['id'=>$relationship->id]) }}" style='float:left; margin-left:8px;'>
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <input type='submit' value='x' class='textButton red' />
+                        <span style='color:grey;'>
+                        {{ $relationship->type->name }}
+                        </span>
+                    </form>
+                @endif
+            @endforeach
+            <div style='float:left;margin-left:8px;'>
             <input type='button' id='showGroupTypesForPerson{{$person->id}}' value='[ + ]' class='textButton showGroupTypesForPerson'  />    
             <input type='button' id='hideGroupTypesForPerson{{$person->id}}' value='[ - ]' class='textButton hideGroupTypesForPerson'  />    
+            </div>
             <div id='listOfGroupTypes{{$person->id}}' class='clear listOfGroupTypes'>
-                <input type='text' /> <input type='button' value='Add Relation' />
+                <form method="POST" action="{{ route('relationship.store') }}">
+                    {{ csrf_field() }}
+                    <select name='relationshipTypeID'>
+                        @foreach ($relationship_types as $relationship_type)
+                            <option value='{{ $relationship_type->id }}'>{{ $relationship_type->name }}</option>
+                        @endforeach
+                    </select>
+                    <input type='hidden' name='primaryPersonID' value='{{ $person_id }}' />
+                    <input type='hidden' name='secondaryPersonID' value='{{ $person->id }}' />
+                    <input type='submit' value='Add Relationship' />
+                </form>
             </div>
         </div>
     @endforeach
